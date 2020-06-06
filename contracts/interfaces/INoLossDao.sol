db096a07e8f2564acc40ed1fa9ecb0ccf0daf019
jonjon <jonjonclark@gmail.com>
2020-06-06 10:55:44 +0200

    Quadratic voting functionality implemented.

--- contracts/interfaces/INoLossDao.sol
+++ contracts/interfaces/INoLossDao.sol
@@ -2,16 +2,18 @@ pragma solidity ^0.5.0;
 
 
 contract INoLossDao {
-  function noLossDeposit(address userAddress) external returns (bool);
+    function noLossDeposit(address userAddress, uint256 amount)
+        external
+        returns (bool);
 
-  function noLossWithdraw(address userAddress) external returns (bool);
+    function noLossWithdraw(address userAddress) external returns (bool);
 
-  function noLossCreateProposal(
-    string calldata proposalHash,
-    address benefactorAddress
-  ) external returns (uint256 newProposalId);
+    function noLossCreateProposal(
+        string calldata proposalHash,
+        address benefactorAddress
+    ) external returns (uint256 newProposalId);
 
-  function noLossWithdrawProposal(address benefactorAddress)
-    external
-    returns (bool);
+    function noLossWithdrawProposal(address benefactorAddress)
+        external
+        returns (bool);
 }
